43ff7b3646959f3f8b08dde879cebe6f49a1b5f9
Abram <acarter6@unca.edu>
2023-03-17 10:13:24 -0400

    quiz2

---
+++ lectures/lecture15/01-style-property-demo/main.js
@@ -0,0 +1,41 @@
+
+function makeRed() {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('#section1').style.backgroundColor = 'red';
+    document.querySelector('#section1').style.color='white';
+    document.querySelector('#section1').style.fontsize = '50px';
+}
+
+function makeBlue() {
+    // your code here...
+    console.log('Change background to blue');
+    document.querySelector('#section2').style.backgroundColor='blue';
+    document.querySelector('#section1').style.Color = 'white';
+}
+
+function makePink() {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector('#section3').style.backgroundColor='pink';
+    document.querySelector('#section1').style.Color = 'white';
+}
+
+function makeOrange() {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('#section4').style.backgroundColor='orange';
+    document.querySelector('#section1').style.Color = 'white';
+}
+
+function multicolor() {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('#section1').style.backgroundColor='red';
+    document.querySelector('#section2').style.backgroundColor='blue';
+    document.querySelector('#section3').style.backgroundColor='pink';
+    document.querySelector('#section4').style.backgroundColor='orange';
+
+
+}
+
